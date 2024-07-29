d8da31337032746aba116bfe5977a1d12f8aa3ca
ysmntw <99477649+ysmntw@users.noreply.github.com>
2024-07-29 07:32:59 -0300

    Update aleatorio.js

---
+++ aleatorio.js
@@ -0,0 +1,8 @@
+constnomes= ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+
+exportfunction aleatorio (lista){
+    constposica= Math.floor(Math.random()* lista.length);
+    returnlista[posicao];
+}
+
+exportconstnome= aleatorio(nomes)
